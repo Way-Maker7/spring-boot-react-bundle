a1f12efed8f2191592f3b46dc32b6a3b2d68baaf
Denis Sabou <Denis_Sabou@gmx.de>
2022-06-08 16:55:55 +0200

    first v

--- backend/src/main/java/com/example/demo/TestController.java
+++ backend/src/main/java/com/example/demo/TestController.java
@@ -1,21 +1,50 @@
 package com.example.demo;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/kanban")
+@RequiredArgsConstructor
+
 public class TestController {
+    private final ToDoServices toDoService;
 
-    private List<String> greetings = Arrays.asList("Hallo", "Moin", "Servus");
 
-    @GetMapping(path="/api/greeting", produces="text/plain")
-    public String hello() {
-        Random rand = new Random();
-        return greetings.get(rand.nextInt(greetings.size()));
+    @GetMapping()
+    public List<ToDo> findAllTodos(){
+        return toDoService.getTodos();
     }
+
+    @PostMapping()
+    public void addToDo(@RequestBody ToDo todo){
+        toDoService.addNew(todo);
+    }
+
+    @PutMapping("/next")
+    public void moveToNextState(@RequestBody ToDo todo){
+        toDoService.changeState(todo);
+    }
+    @PutMapping("/prev")
+    public void setToPrevState(@RequestBody ToDo todo){
+        toDoService.setBackState(todo);
+    }
+
+
+    @DeleteMapping(path ="{todoid}")
+    public void removeToDoTask(@PathVariable String todoid){
+        toDoService.deleteTask(todoid);
+    }
+    @GetMapping(path = "/{todoid}")
+    public ToDo getTodoTask(@PathVariable String todoid){
+        return toDoService.getTaskTodo(todoid);
+    }
+    @PutMapping()
+    public void editTodoTask(@RequestBody ToDo todo){
+        toDoService.editTaskAndDesc(todo);
+    }
+
 
 }
---
+++ backend/src/main/java/com/example/demo/ToDoServices.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+
+public class ToDoServices {
+    private final ToDoRepos toDoRepo;
+
+    public List<ToDo> getTodos() {
+        return toDoRepo.list();
+    }
+
+    public void addNew(ToDo todo) {
+        toDoRepo.add(todo);
+    }
+    public void changeState(ToDo todo) {
+        ToDo wantedToDo = toDoRepo.findToDoById(todo.getId()).orElseThrow();
+        switch (wantedToDo.getStatus()) {
+            case OPEN -> wantedToDo.setStatus(Status.IN_PROGRESS);
+            case IN_PROGRESS -> wantedToDo.setStatus(Status.DONE);
+        }
+    }
+
+    public void setBackState(ToDo todo) {
+        ToDo wantedToDo = toDoRepo.findToDoById(todo.getId()).orElseThrow();
+        switch (wantedToDo.getStatus()) {
+            case IN_PROGRESS -> wantedToDo.setStatus(Status.OPEN);
+            case DONE -> wantedToDo.setStatus(Status.IN_PROGRESS);
+        }
+    }
+
+    public void deleteTask(String todoid) {
+        toDoRepo.delete(todoid);
+    }
+
+    public ToDo getTaskTodo(String todoid) {
+        return toDoRepo.findToDoById(todoid).orElseThrow();
+    }
+
+    public void editTaskAndDesc(ToDo todo) {
+        ToDo taskToEdit = toDoRepo.findToDoById(todo.getId()).orElseThrow();
+        taskToEdit.setTask(todo.getTask());
+        taskToEdit.setDescription(todo.getDescription());
+    }
+}
