07da8c868836962fedf41f8daecc03285d32d365
Way-Maker7 <Denis_Sabou@gmx.de>
2022-06-17 23:07:12 +0200

    separated components with more tasks

--- frontend/src/components/GalleryColumn.tsx
+++ frontend/src/components/GalleryColumn.tsx
@@ -4,12 +4,14 @@ export default function GalleryColumn() {
     return (
         <div className={'Column'}>
             <h3>Status1</h3>
-          <GalleryColumnCard/>
+            <GalleryColumnCard/>
+            <GalleryColumnCard/>
             <h3>Status2</h3>
+            <GalleryColumnCard/>
             <GalleryColumnCard/>
             <h3>Status3</h3>
             <GalleryColumnCard/>
-
+            <GalleryColumnCard/>
 
             </div>
 
